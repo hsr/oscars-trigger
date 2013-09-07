941b19b8d0e12c8a2ad57cda7cdb80869f02f713
Henrique Rodrigues <hsr@cs.ucsd.edu>
2013-09-07 16:12:22 -0700

    Adding documentation to some methods of module runnable

--- oscars_te/runnable/runnable.py
+++ oscars_te/runnable/runnable.py
@@ -18,15 +18,53 @@ class Runnable(threading.Thread):
         self._stopped.clear()
 
     def stop(self): 
+        """
+        This method should be called from the context of 
+        the object creator. It instructs a runnable object
+        to stop its execution. This causes future calls
+        to shouldStop() to return true.
+        """
         self._shouldStop.set()
         return
 
+    def terminate(self): 
+        """
+        This method should be called after the main thread
+        finishes its execution, usually before returning from
+        the run() method implementation. If causes future
+        calls to isStopped() to return true.
+        """
+        self._stopped.set()
+        return
+
     def shouldStop(self):
+        """
+        Returns true if stop() was called, false otherwise.
+        Useful to determine if the Runnable object should
+        stop its execution from an infite loop.
+        """
         return self._shouldStop.is_set()
 
     def isStopped(self):
+        """
+        Returns true if terminate() was called, false otherwise.
+        Useful to determine if the Runnable object had
+        stopped its execution.
+        """
         self._stopped.is_set()
         return
+    
+    def waitInterruptible(self, duration=-1):
+        """
+        Handy function to wait until stop() method is called
+        up to 'duration' seconds. If duration isn't provided
+        or it has a negative value, waits until stop() is
+        called.
+        """
+        if duration < 0:
+            return self._shouldStop.wait(duration)
+        return self._shouldStop.wait()
+
 
     def run(self):
         raise NotImplementedError
