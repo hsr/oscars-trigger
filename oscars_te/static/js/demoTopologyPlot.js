3ad812a06e66f2a91108e417df0bb00994faf41a
Henrique Rodrigues <hsr@cs.ucsd.edu>
2013-09-07 15:59:16 -0700

    Fixing old plot

--- oscars_te/static/js/demoTopologyPlot.js
+++ oscars_te/static/js/demoTopologyPlot.js
@@ -1,6 +1,7 @@
-parseFloodlightTopology("/static/data/demoTopology.json", function() {
+drawUSBackground();
+parseFloodlightTopology("/static/data/topology/base.json", function() {
 	parseFloodlightTopology("/data/topology.json", function () { 
-		drawFloodlightTopology("/static/data/infinerademoLocationMap.csv", function() {
+		drawFloodlightTopology("/static/data/coordinates/base.csv", function() {
 			drawFloodlightCircuits();
 		})
 	})
