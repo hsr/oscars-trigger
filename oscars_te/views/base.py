fdaf13140d55f4c920ac09b275e375704880f195
Henrique Rodrigues <hsr@cs.ucsd.edu>
2013-09-17 18:30:54 -0700

    Adding template for http error 500

--- oscars_te/views/base.py
+++ oscars_te/views/base.py
@@ -1,6 +1,12 @@
+import sys
 from flask import abort, render_template, jsonify
 from jinja2 import TemplateNotFound
 from oscars_te import app
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error  ), 500
 
 @app.errorhandler(404)
 def not_found(e):
