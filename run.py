dd27869244fe65fc0ae1f6ca888c0199890a49eb
Henrique Rodrigues <hsr@cs.ucsd.edu>
2013-09-18 19:25:28 -0700

    Removing unused argument from run.py

--- run.py
+++ run.py
@@ -16,11 +16,9 @@ parser.add_argument('--oscars', action='store', default='localhost:3306',
 parser.add_argument('--listener', action='store', default='localhost:9911',
                     help='OSCARS Listener URL. Default: localhost:9911')
 
-parser.add_argument('--trigger', action='store', default='',
-                    help='sFlow-RT URL. Default: localhost:8008')
-
 parser.add_argument('--debug', action='store_true',
-                    help='Enable debug mode')
+                    help='Enable debug mode '
+                        '(Note: this disable monitor and the trigger itself)')
 
 args = parser.parse_args()
 
@@ -30,7 +28,6 @@ if args.debug:
 
 app.config['controller'] = args.controller;
 app.config['oscars'] = args.oscars;
-app.config['trigger'] = args.trigger;
 app.config['listener'] = args.listener;
 
 if app.debug is False \
@@ -44,20 +41,13 @@ else:
     
 if app.debug is False \
     and not app.config.has_key('trigger_instance') \
-    and len(app.config['trigger']) > 0:
+    and len(app.config['listener']) > 0:
 
     app.config['trigger_instance'] = FloodlightTrigger(
             controller_url=app.config['controller'],
             oscars_url=app.config['oscars'],
             listener_url=app.config['listener'],
             interval=3)
-    # if app.config['trigger'] not in ['localhost', '127.0.0.1']:
-    #     app.config['trigger_instance'] = \
-    #         SFlowTrigger(app.config['trigger'], 
-    #                      app.root_path + '/trigger/sflow-rt/start.sh')
-    # else:
-    #     app.config['trigger_instance'] = SFlowTrigger(app.config['trigger'])
-    
     app.config['trigger_instance'].start()
 else:
     app.config['trigger_instance'] = None;
